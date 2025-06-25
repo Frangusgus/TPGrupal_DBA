ee3603a65e976161d734bbcd46155c8b50ae3040
Gustavo Franco <44347926+Frangusgus@users.noreply.github.com>
2025-06-25 11:26:02 -0300

    Update sublimautomatic.sql
    Corrección de código y añadidas consultas SELECT de tablas

--- sublimautomatic.sql
+++ sublimautomatic.sql
@@ -6,16 +6,16 @@ CREATE TABLE clientes (
   idCliente INT(3) AUTO_INCREMENT PRIMARY KEY,
   nombre VARCHAR(25),
   apellido  VARCHAR(25),
-  telefono INT(7),
-  direccion VARCHAR(25)	
+  telefono VARCHAR(20),
+  direccion VARCHAR(50)
 );
 
 -- Creación de la tabla pedidos
 CREATE TABLE pedidos (
   idPedido INT(3) AUTO_INCREMENT PRIMARY KEY,
-  idCliente INT(3) AUTO_INCREMENT,
+  idCliente INT(3),
   fechaEntrega DATE,
-  estado VARCHAR (10),
+  estado VARCHAR (30),
   FOREIGN KEY (idCliente) REFERENCES clientes(idCliente)
 );
 
@@ -24,53 +24,49 @@ CREATE TABLE productos (
   idProducto INT(3) AUTO_INCREMENT PRIMARY KEY,
   nombre VARCHAR(25),
   precioVenta INT(4),
-  descripcion VARCHAR(30),
+  descripcion VARCHAR(70),
   cantidad INT(3)
 );
-
-
--- Creación de la tabla insumos
-CREATE TABLE insumos (
-  idInsumo INT(3) AUTO_INCREMENT PRIMARY KEY,
-  idProveedor INT(3),
-  nombreinsumo VARCHAR(20),
-  cantidad INT(3),
-  precioUnitario FLAOT(4),
-  FOREIGN KEY (idProveedor) REFERENCES proveedores(idProveedor)
-);
-
 
 -- Creación de la tabla proveedores
 CREATE TABLE proveedores (
   idProveedor INT(3) AUTO_INCREMENT PRIMARY KEY,
   nombreProveedor VARCHAR(25),
-  telefono INT(7),
+  telefono VARCHAR(25),
   mail VARCHAR(30),
   direccion VARCHAR(25)
 );
 
-
+-- Creación de la tabla insumos
+CREATE TABLE insumos (
+  idInsumo INT(3) AUTO_INCREMENT PRIMARY KEY,
+  idProveedor INT(3),
+  nombreinsumo VARCHAR(40),
+  cantidad INT(3),
+  precioUnitario FLOAT(4),
+  FOREIGN KEY (idProveedor) REFERENCES proveedores(idProveedor)
+);
 
 -- Insertar datos de clientes a la tabla clientes
 INSERT INTO clientes (idCliente, nombre, apellido, telefono, direccion) VALUES 
-(1, 'Facundo', 'Palmariciotti', 1523025534, 'Av. Entre Rios 745 6A'),
-('Leandro Javier', 'Fernández', 1522224444, 'Av Cordoba 2886 PB'),
-('Rosana', 'Sosa', 1534042502, 'Vélez Sársfield 680 (Avellaneda)'),
-('Iván', 'Bona', 1534042502, 'Deán Funes 2225 (Merlo)'),
-('Kenneth', 'Loreto', 154893707, 'Av. Las Heras 2840'),
-('Gian', 'Malaspina', 153592088, 'Lavalle 540 2C'),
-('Maria Paz', 'Short', 135039237, 'Islas Malvinas 2681 Timbre 2 (San Andrés)'),
-('Mauro', 'Di Gallo', 1523690874, 'Av Brasil 1021 PB')
+(1, 'Facundo', 'Palmariciotti', '1523025534', 'Av. Entre Rios 745 6A'),
+(2, 'Leandro Javier', 'Fernández', '1522224444', 'Av Cordoba 2886 PB'),
+(3, 'Rosana', 'Sosa', '1534042502', 'Vélez Sársfield 680 (Avellaneda)'),
+(4, 'Iván', 'Bona', '1534042502', 'Deán Funes 2225 (Merlo)'),
+(5, 'Kenneth', 'Loreto', '154893707', 'Av. Las Heras 2840'),
+(6, 'Gian', 'Malaspina', '153592088', 'Lavalle 540 2C'),
+(7, 'Maria Paz', 'Short', '135039237', 'Islas Malvinas 2681 Timbre 2 (San Andrés)'),
+(8, 'Mauro', 'Di Gallo', '1523690874', 'Av Brasil 1021 PB')
 ;
 
 -- Insertar datos de productos a la tabla productos
 INSERT INTO productos (idProducto, nombre, precioVenta, descripcion, cantidad) VALUES
 (1, 'Remera Rosana', 5500, 'Remera Gris con foto de perrito', 1 ),
-('Taza Boca Juniors', 3500, 'Taza de plástico con logo de Boca Juniors', 10),
-('Stickers Facundo', 7000, 'Stickers con ilustraciones de Facundo', 50),
-('Remera Back to the Future', 7500, 'Remera blanca con motivo película Back to the Future', 5),
-('Taza Cumple Bona', 8000, 'Taza para cumpleaños de cliente', 5),
-('Buzo River Campeón', 5000, 'Buzo negro con vinilo de River campeón', 2)
+(2, 'Taza Boca Juniors', 3500, 'Taza de plástico con logo de Boca Juniors', 10),
+(3, 'Stickers Facundo', 7000, 'Stickers con ilustraciones de Facundo', 50),
+(4, 'Remera Back to the Future', 7500, 'Remera blanca con motivo película Back to the Future', 5),
+(5, 'Taza Cumple Bona', 8000, 'Taza para cumpleaños de cliente', 5),
+(6, 'Buzo River Campeón', 5000, 'Buzo negro con vinilo de River campeón', 2)
 ;
 
 -- Insertar datos de pedidos a la tabla pedidos
@@ -86,8 +82,8 @@ INSERT INTO pedidos (idPedido, idCliente, fechaEntrega, estado) VALUES
 -- Insertar datos de proveedores a la tabla proveedores
 INSERT INTO proveedores (idProveedor, nombreProveedor, telefono, mail, direccion) VALUES
 (1, 'DANITEX', '1123991098', 'daniteximportador@gmail.com', 'Tucumán 2980'),
-('DeTintas', '1139512690', 'impo@detintas.com.ar', 'Tucumán 2086'),
-('Once Tex', '1168201924', 'oncetex@gmail.com', 'Junín 495')
+(2, 'DeTintas', '1139512690', 'impo@detintas.com.ar', 'Tucumán 2086'),
+(3, 'Once Tex', '1168201924', 'oncetex@gmail.com', 'Junín 495')
 ;
 
 -- Insertar datos de insumos a la tabla insumos
@@ -105,12 +101,19 @@ INSERT INTO insumos (idInsumo, idProveedor, nombreInsumo, cantidad, precioUnitar
 
 -- Crear una tabla de vista que oculte información del cliente
 CREATE VIEW nombredeCliente AS
-SELECT nombre, apellido, telefono, direccion FROM clientes
+SELECT nombre, apellido, telefono, direccion FROM clientes;
   
 
 -- Crear una tabla de vista que oculte información de proveedores
 CREATE VIEW datosProveedor AS
-SELECT nombreProveedor, telefono, mail, direccion FROM proveedores
+SELECT nombreProveedor, telefono, mail, direccion FROM proveedores;
+
+-- Seleccionar las distintas tablas para ver sus valores
+SELECT * FROM insumos;
+SELECT * FROM pedidos;
+SELECT * FROM proveedores;
+SELECT * FROM productos;
+SELECT * FROM clientes;
 
 -- Obtener el nombre del cliente que realizó la venta con idVenta = 2:
 
